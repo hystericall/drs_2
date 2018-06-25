32ed1820e9ca89190f9f0f9303fb4f98b8c1702e
Bui Son <buison559@gmail.com>
2018-06-25 16:04:49 +0700

    Position and Division creation
    No index css and show html/css yet.

---
+++ app/controllers/divisions_controller.rb
@@ -0,0 +1,39 @@
+class DivisionsController < ApplicationController
+  before_action :is_admin_logged_in, only: %i(new create)
+  before_action :load_division, except: %i(index create new)
+
+  def show; end
+
+  def index
+    @divisions = Division.paginate(page: params[:page], per_page: Settings.index_per_page)
+      .descending
+  end
+
+  def create
+    @division = Division.new division_params
+    if @division.save
+      flash[:success] = t "success"
+      redirect_to @division
+    else
+      flash[:danger] = t "failed"
+      redirect_to root_path
+    end
+  end
+
+  def new
+    @division = Division.new
+  end
+
+  private
+
+  def division_params
+    params.require(:division).permit :name
+  end
+
+  def load_division
+    @division = Division.find_by id: params[:id]
+    return if @division
+    flash[:danger] = t "division_not_found"
+    redirect_to root_path
+  end
+end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,8 @@
 class Position < ApplicationRecord
   has_many :users
+
+  validates :name, length: {maximum: Settings.max_length}, presence: true,
+    uniqueness: {case_sensitive: false}
+
+  scope :descending, ->{order(created_at: :desc)}
 end
